9e7a0a4a7a29792e4a298b1aedb794413a50c4f8
CHEN Jeremy (CM/ESA-CN) <cje3sgh@your.hostname.com>
2019-12-24 09:47:59 +0800

    fdbus-jni.so->libfdbus-jni.so

--- Android.mk
+++ Android.mk
@@ -1,12 +1,20 @@
+#=====================================================================================
+#                   makefile to build fdbus in aosp source tree                      |
+#=====================================================================================
+
+#=====================================================================================
+#                           build libcommon-base.so                                  |
+#=====================================================================================
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE := libcommon-base
 
-FDB_IDL_MSGHDR_H = \<$(LOCAL_PATH)/idl/android_fix/common.base.MessageHeader.pb.h\>
-FDB_IDL_NAMESERVER_H = \<$(LOCAL_PATH)/idl/android_fix/common.base.NameServer.pb.h\>
+FDB_IDL_MSGHDR_H := \<$(LOCAL_PATH)/idl/android_fix/common.base.MessageHeader.pb.h\>
+FDB_IDL_NAMESERVER_H := \<$(LOCAL_PATH)/idl/android_fix/common.base.NameServer.pb.h\>
+FDB_IDL_MACRO_HEAD_FILE := -DFDB_IDL_MSGHDR_H=$(FDB_IDL_MSGHDR_H) -DFDB_IDL_NAMESERVER_H=$(FDB_IDL_NAMESERVER_H)
 
-LOCAL_CPPFLAGS := -frtti -fexceptions -Wno-unused-parameter -D__LINUX__ -DFDB_CFG_SOCKET_PATH=\"/data/local/tmp\" -DCONFIG_DEBUG_LOG -DCONFIG_SOCKET_PEERCRED -DCONFIG_SOCKET_CONNECT_TIMEOUT=0 -DCONFIG_LOG_TO_STDOUT -DFDB_IDL_MSGHDR_H=$(FDB_IDL_MSGHDR_H) -DFDB_IDL_NAMESERVER_H=$(FDB_IDL_NAMESERVER_H)
-LOCAL_CFLAGS := -Wno-unused-parameter -D__LINUX__ -DFDB_CFG_SOCKET_PATH=\"/data/local/tmp\" -DCONFIG_DEBUG_LOG -DCONFIG_SOCKET_PEERCRED -DCONFIG_SOCKET_CONNECT_TIMEOUT=0 -DCONFIG_LOG_TO_STDOUT -DFDB_IDL_MSGHDR_H=$(FDB_IDL_MSGHDR_H) -DFDB_IDL_NAMESERVER_H=$(FDB_IDL_NAMESERVER_H)
+LOCAL_CPPFLAGS := -frtti -fexceptions -Wno-unused-parameter -D__LINUX__ -DFDB_CFG_SOCKET_PATH=\"/data/local/tmp\" -DCONFIG_DEBUG_LOG -DCONFIG_SOCKET_PEERCRED -DCONFIG_SOCKET_CONNECT_TIMEOUT=0 -DCONFIG_LOG_TO_STDOUT $(FDB_IDL_MACRO_HEAD_FILE)
+LOCAL_CFLAGS := -Wno-unused-parameter -D__LINUX__ -DFDB_CFG_SOCKET_PATH=\"/data/local/tmp\" -DCONFIG_DEBUG_LOG -DCONFIG_SOCKET_PEERCRED -DCONFIG_SOCKET_CONNECT_TIMEOUT=0 -DCONFIG_LOG_TO_STDOUT
 
 SRC_FILES := 
 SRC_FILES += $(wildcard $(LOCAL_PATH)/fdbus/*.cpp)
@@ -51,10 +59,13 @@ LOCAL_PROTOC_FLAGS := -I.
 
 include $(BUILD_SHARED_LIBRARY)
 
+#=====================================================================================
+#                             build libfdbus-jni.so                                  |
+#=====================================================================================
 include $(CLEAR_VARS)
-LOCAL_MODULE:= fdbus-jni
-LOCAL_CPPFLAGS := -frtti -fexceptions -Wno-unused-parameter -D__LINUX__ -DFDB_CFG_SOCKET_PATH=\"/data/local/tmp\" -DCONFIG_DEBUG_LOG -DCFG_JNI_ANDROID -DFDB_IDL_MSGHDR_H=$(FDB_IDL_MSGHDR_H) -DFDB_IDL_NAMESERVER_H=$(FDB_IDL_NAMESERVER_H) -DFDB_CFG_KEEP_ENV_TYPE
-LOCAL_CFLAGS := -Wno-unused-parameter -D__LINUX__ -DFDB_CFG_SOCKET_PATH=\"/data/local/tmp\" -DCONFIG_DEBUG_LOG -DCFG_JNI_ANDROID -DFDB_IDL_MSGHDR_H=$(FDB_IDL_MSGHDR_H) -DFDB_IDL_NAMESERVER_H=$(FDB_IDL_NAMESERVER_H) -DFDB_CFG_KEEP_ENV_TYPE
+LOCAL_MODULE:= libfdbus-jni
+LOCAL_CPPFLAGS := -frtti -fexceptions -Wno-unused-parameter -D__LINUX__ -DFDB_CFG_SOCKET_PATH=\"/data/local/tmp\" -DCONFIG_DEBUG_LOG -DCFG_JNI_ANDROID $(FDB_IDL_MACRO_HEAD_FILE) -DFDB_CFG_KEEP_ENV_TYPE
+LOCAL_CFLAGS := -Wno-unused-parameter -D__LINUX__ -DFDB_CFG_SOCKET_PATH=\"/data/local/tmp\" -DCONFIG_DEBUG_LOG -DCFG_JNI_ANDROID
 LOCAL_SRC_FILES:= \
               jni/src/cpp/CJniClient.cpp \
               jni/src/cpp/CJniMessage.cpp \
@@ -70,12 +81,18 @@ LOCAL_C_INCLUDES += \
 
 include $(BUILD_SHARED_LIBRARY)
 
+#=====================================================================================
+#                                build fdbus-jni.jar                                 |
+#=====================================================================================
 include $(CLEAR_VARS)
 LOCAL_SDK_VERSION := current
 LOCAL_MODULE := fdbus-jni
 LOCAL_SRC_FILES := $(call all-java-files-under, jni/src/java/ipc/fdbus)
 include $(BUILD_JAVA_LIBRARY)
 
+#=====================================================================================
+#               build fdbus-java-client.jar (java test client)                       |
+#=====================================================================================
 include $(CLEAR_VARS)
 LOCAL_SDK_VERSION := current
 LOCAL_MODULE := fdbus-java-client
@@ -88,6 +105,9 @@ LOCAL_JAVA_LIBRARIES := fdbus-jni \
             libprotobuf-java-lite
 include $(BUILD_JAVA_LIBRARY)
 
+#=====================================================================================
+#               build fdbus-java-server.jar (java test server)                       |
+#=====================================================================================
 include $(CLEAR_VARS)
 LOCAL_SDK_VERSION := current
 LOCAL_MODULE := fdbus-java-server
@@ -100,11 +120,14 @@ LOCAL_JAVA_LIBRARIES := fdbus-jni \
            libprotobuf-java-lite
 include $(BUILD_JAVA_LIBRARY)
 
+#=====================================================================================
+#                                  build name-server                                 |
+#=====================================================================================
 include $(CLEAR_VARS)
 LOCAL_MODULE:= name-server
 #LOCAL_INIT_RC := fdbus-name-server.rc
-LOCAL_CPPFLAGS := -frtti -fexceptions -Wno-unused-parameter -D__LINUX__ -DFDB_CFG_SOCKET_PATH=\"/data/local/tmp\" -DCONFIG_DEBUG_LOG -DFDB_IDL_MSGHDR_H=$(FDB_IDL_MSGHDR_H) -DFDB_IDL_NAMESERVER_H=$(FDB_IDL_NAMESERVER_H)
-LOCAL_CFLAGS := -Wno-unused-parameter -D__LINUX__ -DFDB_CFG_SOCKET_PATH=\"/data/local/tmp\" -DCONFIG_DEBUG_LOG -DFDB_IDL_MSGHDR_H=$(FDB_IDL_MSGHDR_H) -DFDB_IDL_NAMESERVER_H=$(FDB_IDL_NAMESERVER_H)
+LOCAL_CPPFLAGS := -frtti -fexceptions -Wno-unused-parameter -D__LINUX__ -DFDB_CFG_SOCKET_PATH=\"/data/local/tmp\" -DCONFIG_DEBUG_LOG $(FDB_IDL_MACRO_HEAD_FILE)
+LOCAL_CFLAGS := -Wno-unused-parameter -D__LINUX__ -DFDB_CFG_SOCKET_PATH=\"/data/local/tmp\" -DCONFIG_DEBUG_LOG
 LOCAL_SRC_FILES:= \
                 server/main_ns.cpp \
                 server/CNameServer.cpp \
@@ -119,10 +142,13 @@ LOCAL_SHARED_LIBRARIES := \
 
 include $(BUILD_EXECUTABLE)
 
+#=====================================================================================
+#                                  build host-server                                 |
+#=====================================================================================
 include $(CLEAR_VARS)
 LOCAL_MODULE:= host-server
-LOCAL_CPPFLAGS := -frtti -fexceptions -Wno-unused-parameter -D__LINUX__ -DFDB_CFG_SOCKET_PATH=\"/data/local/tmp\" -DCONFIG_DEBUG_LOG -DFDB_IDL_MSGHDR_H=$(FDB_IDL_MSGHDR_H) -DFDB_IDL_NAMESERVER_H=$(FDB_IDL_NAMESERVER_H)
-LOCAL_CFLAGS := -Wno-unused-parameter -D__LINUX__ -DFDB_CFG_SOCKET_PATH=\"/data/local/tmp\" -DCONFIG_DEBUG_LOG -DFDB_IDL_MSGHDR_H=$(FDB_IDL_MSGHDR_H) -DFDB_IDL_NAMESERVER_H=$(FDB_IDL_NAMESERVER_H)
+LOCAL_CPPFLAGS := -frtti -fexceptions -Wno-unused-parameter -D__LINUX__ -DFDB_CFG_SOCKET_PATH=\"/data/local/tmp\" -DCONFIG_DEBUG_LOG $(FDB_IDL_MACRO_HEAD_FILE)
+LOCAL_CFLAGS := -Wno-unused-parameter -D__LINUX__ -DFDB_CFG_SOCKET_PATH=\"/data/local/tmp\"
 LOCAL_SRC_FILES:= \
                 server/main_hs.cpp \
                 server/CHostServer.cpp \
@@ -135,10 +161,13 @@ LOCAL_SHARED_LIBRARIES := \
 
 include $(BUILD_EXECUTABLE)
 
+#=====================================================================================
+#                             build lssvc (list service)                             |
+#=====================================================================================
 include $(CLEAR_VARS)
 LOCAL_MODULE:= lssvc
-LOCAL_CPPFLAGS := -frtti -fexceptions -Wno-unused-parameter -D__LINUX__ -DCONFIG_DEBUG_LOG -DFDB_IDL_MSGHDR_H=$(FDB_IDL_MSGHDR_H) -DFDB_IDL_NAMESERVER_H=$(FDB_IDL_NAMESERVER_H)
-LOCAL_CFLAGS := -Wno-unused-parameter -D__LINUX__ -DCONFIG_DEBUG_LOG -DFDB_IDL_MSGHDR_H=$(FDB_IDL_MSGHDR_H) -DFDB_IDL_NAMESERVER_H=$(FDB_IDL_NAMESERVER_H)
+LOCAL_CPPFLAGS := -frtti -fexceptions -Wno-unused-parameter -D__LINUX__ -DCONFIG_DEBUG_LOG $(FDB_IDL_MACRO_HEAD_FILE)
+LOCAL_CFLAGS := -Wno-unused-parameter -D__LINUX__ -DCONFIG_DEBUG_LOG
 LOCAL_SRC_FILES:= \
                 server/main_ls.cpp
 LOCAL_SRC_FILES += $(FDB_PROTO_SRC)
@@ -148,10 +177,13 @@ LOCAL_SHARED_LIBRARIES := \
                 libcommon-base
 include $(BUILD_EXECUTABLE)
 
+#=====================================================================================
+#                              build lshost (list host)                              |
+#=====================================================================================
 include $(CLEAR_VARS)
 LOCAL_MODULE:= lshost
-LOCAL_CPPFLAGS := -frtti -fexceptions -Wno-unused-parameter -D__LINUX__ -DCONFIG_DEBUG_LOG -DFDB_IDL_MSGHDR_H=$(FDB_IDL_MSGHDR_H) -DFDB_IDL_NAMESERVER_H=$(FDB_IDL_NAMESERVER_H)
-LOCAL_CFLAGS := -Wno-unused-parameter -D__LINUX__ -DCONFIG_DEBUG_LOG -DFDB_IDL_MSGHDR_H=$(FDB_IDL_MSGHDR_H) -DFDB_IDL_NAMESERVER_H=$(FDB_IDL_NAMESERVER_H)
+LOCAL_CPPFLAGS := -frtti -fexceptions -Wno-unused-parameter -D__LINUX__ -DCONFIG_DEBUG_LOG $(FDB_IDL_MACRO_HEAD_FILE)
+LOCAL_CFLAGS := -Wno-unused-parameter -D__LINUX__ -DCONFIG_DEBUG_LOG
 LOCAL_SRC_FILES:= \
                 server/main_lh.cpp
 LOCAL_SRC_FILES += $(FDB_PROTO_SRC)
@@ -161,10 +193,13 @@ LOCAL_SHARED_LIBRARIES := \
                 libcommon-base
 include $(BUILD_EXECUTABLE)
 
+#=====================================================================================
+#                             build logsvc (log service)                             |
+#=====================================================================================
 include $(CLEAR_VARS)
 LOCAL_MODULE:= logsvc 
-LOCAL_CPPFLAGS := -frtti -fexceptions -Wno-unused-parameter -D__LINUX__ -DCONFIG_DEBUG_LOG -DFDB_IDL_MSGHDR_H=$(FDB_IDL_MSGHDR_H) -DFDB_IDL_NAMESERVER_H=$(FDB_IDL_NAMESERVER_H)
-LOCAL_CFLAGS := -Wno-unused-parameter -D__LINUX__ -DCONFIG_DEBUG_LOG -DFDB_IDL_MSGHDR_H=$(FDB_IDL_MSGHDR_H) -DFDB_IDL_NAMESERVER_H=$(FDB_IDL_NAMESERVER_H)
+LOCAL_CPPFLAGS := -frtti -fexceptions -Wno-unused-parameter -D__LINUX__ -DCONFIG_DEBUG_LOG $(FDB_IDL_MACRO_HEAD_FILE)
+LOCAL_CFLAGS := -Wno-unused-parameter -D__LINUX__ -DCONFIG_DEBUG_LOG
 LOCAL_SRC_FILES:= \
                 server/main_log_server.cpp \
                 server/CLogPrinter.cpp
@@ -176,10 +211,13 @@ LOCAL_SHARED_LIBRARIES := \
 
 include $(BUILD_EXECUTABLE)
 
+#=====================================================================================
+#                           build logviewer (log viewer)                             |
+#=====================================================================================
 include $(CLEAR_VARS)
 LOCAL_MODULE:= logviewer
-LOCAL_CPPFLAGS := -frtti -fexceptions -Wno-unused-parameter -D__LINUX__ -DCONFIG_DEBUG_LOG -DFDB_IDL_MSGHDR_H=$(FDB_IDL_MSGHDR_H) -DFDB_IDL_NAMESERVER_H=$(FDB_IDL_NAMESERVER_H)
-LOCAL_CFLAGS := -Wno-unused-parameter -D__LINUX__ -DCONFIG_DEBUG_LOG -DFDB_IDL_MSGHDR_H=$(FDB_IDL_MSGHDR_H) -DFDB_IDL_NAMESERVER_H=$(FDB_IDL_NAMESERVER_H)
+LOCAL_CPPFLAGS := -frtti -fexceptions -Wno-unused-parameter -D__LINUX__ -DCONFIG_DEBUG_LOG $(FDB_IDL_MACRO_HEAD_FILE)
+LOCAL_CFLAGS := -Wno-unused-parameter -D__LINUX__ -DCONFIG_DEBUG_LOG
 LOCAL_SRC_FILES:= \
                 server/main_log_client.cpp \
                 server/CLogPrinter.cpp
@@ -190,11 +228,14 @@ LOCAL_SHARED_LIBRARIES := \
                 libcommon-base
 include $(BUILD_EXECUTABLE)
 
+#=====================================================================================
+#                            build fdbtest (native test)                             |
+#=====================================================================================
 include $(CLEAR_VARS)
 LOCAL_MODULE:= fdbtest 
 FDB_IDL_EXAMPLE_H = \<$(FDB_IDL_DIR)/common.base.Example.pb.h\>
-LOCAL_CPPFLAGS := -frtti -fexceptions -Wno-unused-parameter -D__LINUX__ -DCONFIG_DEBUG_LOG -DFDB_IDL_MSGHDR_H=$(FDB_IDL_MSGHDR_H) -DFDB_IDL_NAMESERVER_H=$(FDB_IDL_NAMESERVER_H) -DFDB_IDL_EXAMPLE_H=$(FDB_IDL_EXAMPLE_H)
-LOCAL_CFLAGS := -Wno-unused-parameter -D__LINUX__ -DCONFIG_DEBUG_LOG -DFDB_IDL_MSGHDR_H=$(FDB_IDL_MSGHDR_H) -DFDB_IDL_NAMESERVER_H=$(FDB_IDL_NAMESERVER_H)
+LOCAL_CPPFLAGS := -frtti -fexceptions -Wno-unused-parameter -D__LINUX__ -DCONFIG_DEBUG_LOG $(FDB_IDL_MACRO_HEAD_FILE) -DFDB_IDL_EXAMPLE_H=$(FDB_IDL_EXAMPLE_H)
+LOCAL_CFLAGS := -Wno-unused-parameter -D__LINUX__ -DCONFIG_DEBUG_LOG
 LOCAL_SRC_FILES:= \
                 example/client_server_object.cpp \
                 idl/common.base.Example.proto
